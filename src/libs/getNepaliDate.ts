048e7319de27c6055076500ffe738d7ed54d6c5d
Romus Shrestha <sromus498@gmail.com>
2025-01-29 15:56:56 +0545

    added additional date data and changed variable names

--- src/libs/getNepaliDate.ts
+++ src/libs/getNepaliDate.ts
@@ -1,26 +1,53 @@
 import { adToBs } from "@sbmdkl/nepali-date-converter";
+import getNepaliDay from "./getNepaliDay";
+import getBsMonth from "./getBsMonth";
+import getData from "./getData";
 
-export default  function getNepaliDate() {
+export default async function getNepaliDate() {
     try {
 
-        const today = new Date();
-        today.getDate();
-        console.log(today.toString());
-        const year= today.getFullYear().toString();
-        const month= (today.getMonth()+1).toString();
-        const date= today.getDate().toString();
-        const adDate= `${year}-${month}-${date}`;
-        console.log(today.getMonth().toString());
 
+        const today = new Date();
+
+        // date for AD calendar
+        const enYear = today.getFullYear().toString();
+        const enMonth = (today.getMonth() + 1).toString();
+        const enDate = today.getDate().toString();
+        const adDate = `${enYear}-${enMonth}-${enDate}`;
+
+        // date for BS calendar from package
         const adToBsDate = adToBs(adDate);
-        console.log("here:", adToBsDate);
-        console.log(adToBsDate);
         const bsDate = adToBsDate.toString().split("-");
-        console.log(bsDate);
-        return {bsDate,year,month,date,adDate};
-        
-        
+        const [bsYear, bsMonth, bsDay] = bsDate;
+        const nepaliDay = getNepaliDay();
+        const nepaliMonth = getBsMonth(bsMonth);
+
+        // get english month in string ; eg: Janurary
+        const englishMonth = today.toLocaleDateString("en-US", { month: "long" });
+
+        // fetch tithi, holidays and events fromapi
+        const {singleDayData, bratabandhaData, marriageData} = await getData(
+            today.getDate().toString().padStart(2, "0"),
+            bsMonth,
+            bsYear
+        )
+
+        return { 
+            bsDate, 
+            enYear, 
+            enMonth, 
+            enDate, 
+            englishMonth, 
+            nepaliDay, 
+            nepaliMonth, 
+            singleDayData,
+            bratabandhaData,
+            marriageData
+        };
+
+
     } catch (e) {
+        return null;
         console.log(e.message);
     }
 }
