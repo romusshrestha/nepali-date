bc6e1e2dac720c96277ed9851202638016a88b3b
Romus Shrestha <sromus498@gmail.com>
2025-01-29 15:57:50 +0545

    added feature to display bratabandha, marrige data nad single day data

--- src/features/date-display.tsx
+++ src/features/date-display.tsx
@@ -2,8 +2,8 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "~components/ui/card"
 // import { Separator } from "@/components/ui/separator"
-import { Calendar, Clock } from "lucide-react"
-import { useState } from "react";
+import { Calendar, Clock, Loader, Loader2Icon, LoaderCircle, LoaderIcon, LoaderPinwheel } from "lucide-react"
+import { useEffect, useState } from "react";
 import getData from "~libs/getData";
 import getNepaliDate from "~libs/getNepaliDate";
 import getNepaliDay from "~libs/getNepaliDay";
@@ -16,66 +16,130 @@ interface Event {
   time: string
   title: string
 }
+interface IDay {
+  n: string;
+  e: string;
+  t: string;
+  f: string;
+  h: boolean;
+  d: number
+}
+interface INepaliDate {
+  bsDate: string[];
+  enYear: string;
+  enMonth: string;
+  enDate: string;
+  englishMonth: string;
+  nepaliDay: string;
+  nepaliMonth: string;
+  singleDayData: IDay
+  bratabandhaData: string;
+  marriageData: string;
+}
 
 export default function DateDisplay() {
-  const [tithi, setTithi] = useState('');
-  const [nepaliDay, setNepaliDay] = useState('');
-  const [nepaliMonth, setNepaliMonth] = useState('');
-  const { bsDate, year, month, date, adDate } = getNepaliDate();
-  const getDateDatas = async () => {
-    const fetchedTithi = await getData('01', '1', '2081');
-    console.log(month);
-    const nepMonth= month
-    console.log("nepmonth",nepMonth);
-    const convertedMonth= getBsMonth(nepMonth);
-    console.log(convertedMonth);
-    
-    const convertedDay = getNepaliDay();
-    console.log(tithi);
-    setNepaliMonth(convertedMonth);
-    setNepaliDay(convertedDay);
-    setTithi(fetchedTithi);
-  }
-  getDateDatas();
+  const [nepaliDate, setNepaliDate] = useState<INepaliDate | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dateData = await getNepaliDate();
+        if (dateData) {
+          console.log("Date Data: ", dateData);
+          setNepaliDate(dateData);
+        }
+      } catch (error) {
+        console.error("Error fetching Nepali date:", error)
+      }
+    }
+    fetchData();
+  }, [])
+
 
 
   // Example events, replace with actual events for the day
-  const events: Event[] = [
-    { time: "११:००", title: "टीम मिटिङ" },
-    { time: "१४:३०", title: "लन्च ब्रेक" },
-    { time: "१६:००", title: "प्रोजेक्ट रिभ्यु" },
-  ]
 
+
+  if (!nepaliDate) {
+    return (<>
+      <Card className="w-80 bg-red-50 rounded-lg overflow-hidden mx-2 my-2">
+        <CardHeader className="bg-red-800 text-white ">
+          <CardTitle className="text-center font-bold text-2xl font-sans  gap-2">
+            Loading Data
+            <LoaderPinwheel className="ml-2 h-6 w-6  animate-spin duration-1000 inline" />
+          </CardTitle>
+        </CardHeader>
+
+      </Card>
+    </>);
+  }
   return (
     <div >
       {/* <div className={inter.className}> */}
       <Card className="w-80 bg-red-50 rounded-lg overflow-hidden mx-2 my-2">
         <CardHeader className="bg-red-800 text-white">
-          <CardTitle className="text-center font-bold text-2xl font-sans"> {nepaliMonth} {bsDate[2]} {bsDate[0]}</CardTitle>
-          <p className="text-center text-sm font-sans">{adDate}</p>
-          <p className="text-center text-lg mt-2 font-sans">{nepaliDay}</p>
-          <p className="text-center text-lg mt-2 font-sans">{tithi}</p>
+          <CardTitle className="text-center font-bold text-2xl font-sans"> {nepaliDate.nepaliMonth} {nepaliDate.singleDayData.n}, {nepaliDate.bsDate[0]}</CardTitle>
+          <p className="text-center text-sm font-sans">{nepaliDate.englishMonth.slice(0, 3)} {nepaliDate.enDate}, {nepaliDate.enYear}</p>
+          <p className="text-center text-sm mt-2 font-sans">{nepaliDate.singleDayData.t}</p>
+          <p className="text-center text-lg mt-2 font-sans">{nepaliDate.nepaliDay}</p>
         </CardHeader>
         <CardContent className="p-4">
-          <div className="flex items-center mb-4">
+          <div className="flex items-center mb-2">
             <Calendar className="mr-2 h-5 w-5 text-red-800" />
-            <h2 className="text-lg font-semibold font-sans">आजको कार्यक्रमहरू</h2>
+            <h2 className="text-lg font-semibold font-sans">आजका पर्वहरू</h2>
           </div>
-          {events.length > 0 ? (
-            <ul className="space-y-3">
-              {events.map((event, index) => (
+          {nepaliDate.singleDayData.f ? (
+            <ul className="px-3 space-y-3 text-sm">
+              {nepaliDate.singleDayData.f.split(", ").map((festival, index) => (
                 <li key={index} className="flex items-start">
-                  <Clock className="mr-2 h-4 w-4 mt-1 text-red-800" />
-                  <div>
-                    <span className="font-semibold font-sans">{event.time}</span>
-                    <span className="mx-2">-</span>
-                    <span className="font-sans">{event.title}</span>
-                  </div>
+                  <span className="font-semibold text-red-800">
+                    •
+                  </span>
+                  <span className="ml-2">{festival}</span>
                 </li>
               ))}
             </ul>
           ) : (
-            <p className="text-center text-gray-500 italic font-sans">कुनै कार्यक्रम छैन</p>
+            <p className="text-center text-gray-500 italic font-sans"> आज कुनै पर्वहरू छैन</p>
+          )}
+
+          <div className="flex items-center mb-2 mt-2">
+            <Calendar className="mr-2 h-5 w-5 text-red-800" />
+            <h2 className="text-lg font-semibold font-sans">ब्रतबन्ध मुर्हुतहरू</h2>
+          </div>
+          {nepaliDate.bratabandhaData ? (
+
+            <ul className="px-3 text-sm space-y-3">
+              <li className="flex items-start">
+                <span className="font-semibold text-red-800">
+                  •
+                </span>
+                <span className="ml-2">
+                  {nepaliDate.bratabandhaData}
+                </span>
+              </li>
+            </ul>
+          ) : (
+            <p className="text-center text-gray-500 italic font-sans"> आज कुनै पर्वहरूछैन</p>
+          )}
+
+          <div className="flex items-center mb-2 mt-2">
+            <Calendar className="mr-2 h-5 w-5 text-red-800" />
+            <h2 className="text-lg font-semibold font-sans">विवाह मुर्हुतहरू</h2>
+          </div>
+          {nepaliDate.marriageData? (
+
+            <ul className="px-3 text-sm space-y-3">
+              <li className="flex items-start">
+                <span className="font-semibold text-red-800">
+                  •
+                </span>
+                <span className="ml-2">
+                  {nepaliDate.marriageData}
+                </span>
+              </li>
+            </ul>
+          ) : (
+            <p className="text-center text-gray-500 italic font-sans"> आज कुनै पर्वहरूछैन</p>
           )}
         </CardContent>
         <div className="absolute inset-0 -z-10 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCI+CjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iI2ZmZWJlZSI+PC9yZWN0Pgo8cGF0aCBkPSJNMzAgMzBMMzAgMEw2MCAzMEw2MCA2MEwzMCAzMEwwIDYwTDAgMzBMMzAgMFoiIGZpbGw9IiNmZmQ1ZDUiPjwvcGF0aD4KPC9zdmc+')] opacity-10" />
